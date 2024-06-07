7f1cc81304b3300a9edc505a9d05f76db0033202
Vishal Yadav <vishalyxo@gmail.com>
2024-06-07 11:41:24 +0530

    Adde Vite proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		open: true,
-	},
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/": "https://erp-backend-zeta.vercel.app",
+    },
+    open: true,
+  },
 });
