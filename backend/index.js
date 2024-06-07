b04a2a9bfb1c56815c25e51b1331bbc963871b1b
Vishal Yadav <vishalyxo@gmail.com>
2024-06-07 15:43:17 +0530

    Up

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 let corsOption = {
-  origin: "https://erp-sand.vercel.ap",
+  origin: "https://erp-vshall.vercel.app",
   methods: "GET,POST,PATCH,PUT,DELETE",
   credentials: true,
 };
