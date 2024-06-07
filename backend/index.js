52198e8cda4f5bb328d0b6df0eb235f5e49e171e
Vishal Yadav <vishalyxo@gmail.com>
2024-06-07 11:32:41 +0530

    Added Cors Policy

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,36 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 // const bodyParser = require("body-parser")
-const app = express()
-const Routes = require("./routes/route.js")
+const app = express();
+const Routes = require("./routes/route.js");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+let corsOption = {
+  origin: "https://erp-sand.vercel.app",
+  methods: "GET,POST,PATCH,PUT,DELETE",
+  credentials: true,
+};
 // app.use(bodyParser.json({ limit: '10mb', extended: true }))
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
-app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(express.json({ limit: "10mb" }));
+app.use(cors(corsOption));
 
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
 
-app.use('/', Routes);
+app.use("/", Routes);
 
 app.listen(PORT, () => {
-    console.log(`Server started at port no. ${PORT}`)
-})
+  console.log(`Server started at port no. ${PORT}`);
+});
